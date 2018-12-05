a04dc756140718c44dd58cd69d46420df2ad8947
tianbo <tianbo@xiaomi.com>
2018-12-05 09:26:26 +0800

    updated css

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,5 +1,6 @@
 function startPurchaseV1() {
     var num = 100;
+    $("#txt_output").empty();
     for (var i = 1; i <= num; i++) {
          $.ajax({
                type:"POST",
@@ -7,10 +8,10 @@ function startPurchaseV1() {
                dataType:"json",
                data:{userId:i, productId:2018112815220001},
                success:function(result) {
-                   $("#txt_output").append(result.data+": "+result.message + "\n");
+                   $("#txt_output").append(result.data+": "+result.message + "<br/>");
                },
                error:function(jqXHR){
-                   $("#txt_output").append("发生错误:"+jqXHR.status+"\n\n");
+                   $("#txt_output").append("发生错误:"+jqXHR.status+"<br/>");
                }
            });
     }
@@ -18,6 +19,7 @@ function startPurchaseV1() {
 
 function startPurchaseV2() {
     var num = 100;
+        $("#txt_output").empty();
         for (var i = 1; i <= num; i++) {
              $.ajax({
                    type:"POST",
@@ -25,16 +27,17 @@ function startPurchaseV2() {
                    dataType:"json",
                    data:{userId:i, productId:2018112815220002},
                    success:function(result) {
-                       $("#txt_output").append(result.data+": "+result.message + "\n");
+                       $("#txt_output").append(result.data+": "+result.message + "<br/>");
                    },
                    error:function(jqXHR){
-                       $("#txt_output").append("发生错误:"+jqXHR.status+"\n\n");
+                       $("#txt_output").append("发生错误:"+jqXHR.status+"<br/>");
                    }
                });
         }
 }
 
 function startPurchaseV3() {
+    $("#txt_output").empty();
     var num = 100;
         for (var i = 1; i <= num; i++) {
              $.ajax({
@@ -43,10 +46,10 @@ function startPurchaseV3() {
                    dataType:"json",
                    data:{userId:i, productId:2018112815220003},
                    success:function(result) {
-                       $("#txt_output").append(result.data+": "+result.message + "\n");
+                       $("#txt_output").append(result.data+": "+result.message + "<br/>");
                    },
                    error:function(jqXHR){
-                       $("#txt_output").append("发生错误:"+jqXHR.status+"\n\n");
+                       $("#txt_output").append("发生错误:"+jqXHR.status+"<br/>");
                    }
                });
         }
@@ -54,6 +57,7 @@ function startPurchaseV3() {
 
 function startPurchaseV4() {
     var num = 100;
+        $("#txt_output").empty();
         for (var i = 1; i <= num; i++) {
              $.ajax({
                    type:"POST",
@@ -61,10 +65,10 @@ function startPurchaseV4() {
                    dataType:"json",
                    data:{userId:i, productId:2018112815220004},
                    success:function(result) {
-                       $("#txt_output").append(result.data+": "+result.message + "\n");
+                       $("#txt_output").append(result.data+": "+result.message + "<br/>");
                    },
                    error:function(jqXHR){
-                       $("#txt_output").append("发生错误:"+jqXHR.status+"\n\n");
+                       $("#txt_output").append("发生错误:"+jqXHR.status+"<br/>");
                    }
                });
         }
